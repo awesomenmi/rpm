552ee420888ce4549d82fbcab210584d0e6b5e45
awesomenmi <walkermr@yandex.ru>
2020-06-14 14:58:37 +0300

    changed rrpm.sh

--- rpmm.sh
+++ rpmm.sh
@@ -3,15 +3,15 @@
 yum install -y \
   redhat-lsb-core \
   wget \
-  rpmdevtoools \
+  rpmdevtools \
   rpm-build \
   createrepo \
   yum-utils \
   gcc
 
 # get and extract nginx rpm source package
-wget https://nginx.org/packages/centos/7/SRPMS/nginx-1.14.1-1.el7_4.ngx.src.rpm
-rpm -i nginx-1.14.1-1.el7_4.ngx.src.rpm
+wget https://nginx.org/packages/centos/7/SRPMS/nginx-1.18.0-1.el7.ngx.src.rpm
+rpm -i nginx-1.18.0-1.el7.ngx.src.rpm
 
 # install nginx dependencies
 yum-builddep /root/rpmbuild/SPECS/nginx.spec -y
@@ -19,19 +19,19 @@ wget https://www.openssl.org/source/latest.tar.gz
 tar -xvf latest.tar.gz --directory /usr/lib
 
 # add openssl to nginx configuration options
-sed -i 's|--with-debug|--with-openssl=/usr/lib/openssl-1.1.1a|' /root/rpmbuild/SPECS/nginx.spec
+sed -i 's|--with-debug|--with-openssl=/usr/lib/openssl-1.1.1g|' /root/rpmbuild/SPECS/nginx.spec
 
 # build new nginx package with updated spec
 rpmbuild --bb /root/rpmbuild/SPECS/nginx.spec
 
 # install and configure nginx from newrly created rpm package
-yum localinstall -y /root/rpmbuild/RPMS/x86_64/nginx-1.14.1-1.el7_4.ngx.x86_64.rpm
+yum localinstall -y /root/rpmbuild/RPMS/x86_64/nginx-1.18.0-1.el7.ngx.x86_64.rpm 
 sed -i '/index  index.html index.htm;/a autoindex on;' /etc/nginx/conf.d/default.conf
 systemctl enable --now nginx
 
 # create rpm repo
 mkdir /usr/share/nginx/html/repo
-cp /root/rpmbuild/RPMS/x86_64/nginx-1.14.1-1.el7_4.ngx.x86_64.rpm /usr/share/nginx/html/repo/
+cp /root/rpmbuild/RPMS/x86_64/nginx-1.18.0-1.el7.ngx.x86_64.rpm /usr/share/nginx/html/repo/
 createrepo /usr/share/nginx/html/repo/
 
 # add rpm repo to available list
